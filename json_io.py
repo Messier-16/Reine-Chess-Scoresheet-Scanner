5441fc9c5c7b5f39ebe4416bcf2719e7b646093f
alexfung018 <46287580+alexfung018@users.noreply.github.com>
2019-01-23 23:21:06 -0800

    Add files via upload

--- json_io.py
+++ json_io.py
@@ -1,18 +1,35 @@
 #!flask/bin/python
 
 from flask import Flask, render_template, request
+import cv2 as cv
 import base64
+import numpy as np
+from PIL import Image
+import io
 
 # save wherever you want
 img_location = 'C:\\Users\\alexf\\desktop\\reine\\base64_test.png'
 
 
 # go from base64 encoding to choice location
-def string_to_saved_image(base64_string):
+def string_to_saved_image_old(base64_string):
     split = base64_string.split(',', 1)
     byte_string = bytes(split[1], 'utf-8')
     with open(img_location, 'wb') as fh:
         fh.write(base64.decodebytes(byte_string))
+
+
+def get_numpy(base64_string):
+    split = base64_string.split(',', 1)
+    byte_file = bytes(split[1], 'utf-8')
+    file = base64.decodebytes(byte_file)
+
+    # finding the dimensions
+    im = Image.open(io.BytesIO(file))
+    arr = np.array(im)[:, :, 0]
+    w = 550
+    h = 850
+    return cv.resize(arr, (w, h))
 
 
 app = Flask(__name__)
@@ -28,7 +45,7 @@ def output():
 def worker():
     # read json + reply
     data = request.get_json(force=True)
-    string_to_saved_image(data)
+    numpy_img = get_numpy(data)
     return data
 
 
