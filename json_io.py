f49bf588bd1fa5d2f78613f74b66b1eb69612679
alexfung018 <46287580+alexfung018@users.noreply.github.com>
2019-01-26 12:04:47 -0800

    Add files via upload

--- json_io.py
+++ json_io.py
@@ -1,24 +1,11 @@
-#!flask/bin/python
-
 from flask import Flask, render_template, request
-import cv2 as cv
 import base64
 import numpy as np
 from PIL import Image
 import io
 
-# save wherever you want
-img_location = 'C:\\Users\\alexf\\desktop\\reine\\base64_test.png'
 
-
-# go from base64 encoding to choice location
-def string_to_saved_image_old(base64_string):
-    split = base64_string.split(',', 1)
-    byte_string = bytes(split[1], 'utf-8')
-    with open(img_location, 'wb') as fh:
-        fh.write(base64.decodebytes(byte_string))
-
-
+# from base64 to numpy
 def get_numpy(base64_string):
     split = base64_string.split(',', 1)
     byte_file = bytes(split[1], 'utf-8')
@@ -27,9 +14,7 @@ def get_numpy(base64_string):
     # finding the dimensions
     im = Image.open(io.BytesIO(file))
     arr = np.array(im)[:, :, 0]
-    w = 550
-    h = 850
-    return cv.resize(arr, (w, h))
+    return arr
 
 
 app = Flask(__name__)
@@ -46,7 +31,8 @@ def worker():
     # read json + reply
     data = request.get_json(force=True)
     numpy_img = get_numpy(data)
-    return data
+    message, game_res, moves = 'It worked', '*', data  # to be updated
+    return message + 'delimiter' + game_res + 'delimiter' + moves
 
 
 if __name__ == '__main__':
